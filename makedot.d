993538716fd3af5f0f6508f8359fd053f3157451
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-05-11 20:27:35 +0000

    makedot: Make reduction optional (--reduce)

--- makedot.d
+++ makedot.d
@@ -3,6 +3,7 @@ module makedot;
 
 import std.exception;
 import std.file;
+import std.getopt;
 import std.path;
 
 import ae.utils.json;
@@ -13,35 +14,44 @@ import common;
 
 void main(string[] args)
 {
-	enforce(args.length == 2, "Usage: " ~ args[0] ~ " FILE.dbuildstat");
+	enforce(args.length >= 2, "Usage: " ~ args[0] ~ " [--reduce] FILE.dbuildstat");
+	bool reduce;
+	getopt(args,
+		"reduce", &reduce,
+	);
+
 	auto modules = args[1].readText().jsonParse!(Module[])();
 	bool[string][string] deps;
 	foreach (m; modules)
 	{
 		deps[m.name] = null;
 		foreach (i; m.deps.keys)
-			deps[m.name][i] = true;
+			if (i != "object")
+				deps[m.name][i] = true;
 	}
 
 	auto allModules = deps.keys;
 
-	// Remove redundant edges.
-	// Attempts to preserve the general purpose of the graph,
-	// while removing most edges.
-	bool changed;
-	do
+	if (reduce)
 	{
-		changed = false;
-		foreach (m0; allModules)
-			foreach (m1; allModules)
-				foreach (m2; allModules)
-				if (m1 in deps[m0] && m2 in deps[m1] && m2 in deps[m0])
-				{
-					deps[m0].remove(m2);
-					changed = true;
-				}
+		// Remove redundant edges.
+		// Attempts to preserve the general purpose of the graph,
+		// while removing most edges.
+		bool changed;
+		do
+		{
+			changed = false;
+			foreach (m0; allModules)
+				foreach (m1; allModules)
+					foreach (m2; allModules)
+					if (m1 in deps[m0] && m2 in deps[m1] && m2 in deps[m0])
+					{
+						deps[m0].remove(m2);
+						changed = true;
+					}
 
-	} while (changed);
+		} while (changed);
+	}
 
 	StringBuilder s;
 	s.put("digraph {\n");
