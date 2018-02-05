2283fa52076319d1b35e4978d95c2c626add007c
Vladimir Panteleev <git@thecybershadow.net>
2018-02-05 14:39:13 +0000

    makesvg: Scale dynamically; add configurable width / row height

--- makesvg.d
+++ makesvg.d
@@ -8,96 +8,108 @@ import std.file;
 import std.path;
 import std.string;
 
+import ae.utils.funopt;
 import ae.utils.json;
+import ae.utils.main;
 import ae.utils.xml;
 
 import common;
 
-void main(string[] args)
+void makesvg(
+	Parameter!(string, "FILE.dbuildstat", "inputFile") fileName,
+	Option!int width = 700,
+	Option!int rowHeight = 30,
+)
 {
-	enforce(args.length == 2, "Usage: " ~ args[0] ~ " FILE.dbuildstat");
-	auto modules = args[1].readText().jsonParse!(Module[])();
+	auto modules = fileName.readText().jsonParse!(Module[])();
 	modules.sort!`a.name < b.name`();
 
-	enum WIDTH = 700;
-	enum CENTER = WIDTH/2;
-	enum ROW_HEIGHT = 30;
-	enum TOP = ROW_HEIGHT;
-	enum PIXELS_PER_SECOND = 300;
-	enum TICKS_PER_PIXEL = TICKS_PER_SECOND / PIXELS_PER_SECOND;
-	enum TEXT_X = CENTER + ROW_HEIGHT/2;
-	enum TEXT_SIZE = ROW_HEIGHT*3/4;
-	enum TEXT_HEIGHT = TEXT_SIZE;
+	ulong longestTime = 0;
+	foreach (m; modules)
+		foreach (metric, time; m.bestTime)
+			if (longestTime < time)
+				longestTime = time;
 
-	auto seconds = CENTER/PIXELS_PER_SECOND;
+	auto center = width / 2;
+	auto ticksPerPixel = cast(ulong)(longestTime * 1.1 / center);
+	auto pixelsPerSecond = TICKS_PER_SECOND / ticksPerPixel;
+	auto svgTop = rowHeight;
+	// auto ticksPerPixel = TICKS_PER_SECOND / pixelsPerSecond;
+	auto textX = center + rowHeight/2;
+	auto textSize = rowHeight*3/4;
+	auto textHeight = textSize;
+
+	auto seconds = center/pixelsPerSecond;
 
 	auto svg = newXml().svg();
 	svg.xmlns = "http://www.w3.org/2000/svg";
 	svg["version"] = "1.1";
-	svg.width  = text(WIDTH);
-	svg.height = text(TOP + (modules.length + 2 + Metric.max) * ROW_HEIGHT);
+	svg.width  = text(width);
+	svg.height = text(svgTop + (modules.length + 2 + Metric.max) * rowHeight);
 
 	auto defs = svg.defs();
 	auto grad = defs.linearGradient(["id" : "fade", "x1" : "0%", "y1" : "0%", "x2" : "0%", "y2" : "100%"]);
 	grad.stop(["offset" :   "0%", "style" : "stop-color: rgb(255, 255, 255); stop-opacity: 0.3"]);
 	grad.stop(["offset" : "100%", "style" : "stop-color: rgb(255, 255, 255); stop-opacity: 0"  ]);
 
-	static void line(XmlBuildNode g, double x1, double y1, double x2, double y2) { g.line(["x1" : text(x1), "y1" : text(y1), "x2" : text(x2), "y2" : text(y2)]); }
-	static void hline(XmlBuildNode g, double x1, double x2, double y) { line(g, x1, y, x2, y); }
-	static void vline(XmlBuildNode g, double x, double y1, double y2) { line(g, x, y1, x, y2); }
-	static void label(XmlBuildNode g, double x, double y, string s) { g.text(["x" : text(x), "y" : text(y + TEXT_HEIGHT)]) = s; }
-	static void rect(XmlBuildNode g, double x, double y, double w, double h, string fill) { g.rect(["x" : text(x), "y" : text(y), "width" : text(w), "height" : text(h), "fill" : fill]); }
-	static void shadedRect(XmlBuildNode g, double x, double y, double w, double h, string fill) { rect(g, x, y, w, h, fill); rect(g, x, y, w, h, "url(#fade)"); }
+	void line(XmlBuildNode g, double x1, double y1, double x2, double y2) { g.line(["x1" : text(x1), "y1" : text(y1), "x2" : text(x2), "y2" : text(y2)]); }
+	void hline(XmlBuildNode g, double x1, double x2, double y) { line(g, x1, y, x2, y); }
+	void vline(XmlBuildNode g, double x, double y1, double y2) { line(g, x, y1, x, y2); }
+	void label(XmlBuildNode g, double x, double y, string s) { g.text(["x" : text(x), "y" : text(y + textHeight)]) = s; }
+	void rect(XmlBuildNode g, double x, double y, double w, double h, string fill) { g.rect(["x" : text(x), "y" : text(y), "width" : text(w), "height" : text(h), "fill" : fill]); }
+	void shadedRect(XmlBuildNode g, double x, double y, double w, double h, string fill) { rect(g, x, y, w, h, fill); rect(g, x, y, w, h, "url(#fade)"); }
 
 	auto grid = svg.g();
 	grid.style = "stroke:rgb(200,200,200); stroke-width:2";
 	foreach (y; 0..modules.length+1)
-		hline(grid, 0, WIDTH, TOP + y * ROW_HEIGHT);
+		hline(grid, 0, width, svgTop + y * rowHeight);
 	foreach (x; 0..seconds+1)
-		vline(grid, CENTER - x*PIXELS_PER_SECOND, TOP, TOP + modules.length * ROW_HEIGHT);
+		vline(grid, center - x*pixelsPerSecond, svgTop, svgTop + modules.length * rowHeight);
 
 	auto timeLabels = svg.g();
-	timeLabels.style = "text-anchor: middle; font-size: " ~ text(TEXT_SIZE) ~ "px";
+	timeLabels.style = "text-anchor: middle; font-size: " ~ text(textSize) ~ "px";
 	timeLabels.title() = "(seconds)";
 	foreach (x; 0..seconds+1)
-		label(timeLabels, CENTER - x*PIXELS_PER_SECOND, 0, text(x));
+		label(timeLabels, center - x*pixelsPerSecond, 0, text(x));
 
 	enum string[Metric.max] METRIC_NAMES  = ["Parse imports", "Import", "Parse", "Compile"];
 	enum string[Metric.max] METRIC_COLORS = ["#2222FF", "#00AAAA", "#00CC00", "#FF0000"];
 
 	auto boxes = svg.g();
 	auto labels = svg.g();
-	labels.style = "font-size: " ~ text(TOP * 3 / 4) ~ "px";
+	labels.style = "font-size: " ~ text(svgTop * 3 / 4) ~ "px";
 	foreach (y, m; modules)
 	{
-		auto rowY = TOP + y * ROW_HEIGHT;
+		auto rowY = svgTop + y * rowHeight;
 		foreach_reverse(metric, time; m.bestTime)
 		{
 			auto g = boxes.g();
 			long relTime = time - (metric ? m.bestTime[metric-1] : 0);
 			if (relTime < 0) continue;
 			g.title() = format("%s - %5.3f seconds", METRIC_NAMES[metric], relTime * 1.0 / TICKS_PER_SECOND);
-			auto w = time * 1.0 / TICKS_PER_PIXEL;
-			shadedRect(g, CENTER - w, rowY, w, ROW_HEIGHT, METRIC_COLORS[metric]);
+			auto w = time * 1.0 / ticksPerPixel;
+			shadedRect(g, center - w, rowY, w, rowHeight, METRIC_COLORS[metric]);
 		}
 
-		label(labels, TEXT_X, rowY, m.name);
+		label(labels, textX, rowY, m.name);
 	}
 
 	auto legend = svg.g();
 	auto legendText = svg.g();
 	legend.style = "stroke:black; stroke-width:1";
-	legendText.style = "font-size: " ~ text(TOP * 3 / 4) ~ "px";
+	legendText.style = "font-size: " ~ text(svgTop * 3 / 4) ~ "px";
 
-	auto legendY = TOP + (modules.length + 1) * ROW_HEIGHT;
-	label(legendText, TEXT_X, legendY, "Legend:");
-	legendY += ROW_HEIGHT;
+	auto legendY = svgTop + (modules.length + 1) * rowHeight;
+	label(legendText, textX, legendY, "Legend:");
+	legendY += rowHeight;
 
 	foreach (metric; Metric.init..Metric.max)
 	{
-		shadedRect(legend, CENTER - ROW_HEIGHT*1/4, legendY + metric * ROW_HEIGHT + ROW_HEIGHT*1/4, ROW_HEIGHT/2, ROW_HEIGHT/2, METRIC_COLORS[metric]);
-		label(legendText, TEXT_X, legendY + metric * ROW_HEIGHT, METRIC_NAMES[metric]);
+		shadedRect(legend, center - rowHeight*1/4, legendY + metric * rowHeight + rowHeight*1/4, rowHeight/2, rowHeight/2, METRIC_COLORS[metric]);
+		label(legendText, textX, legendY + metric * rowHeight, METRIC_NAMES[metric]);
 	}
 
-	args[1].setExtension("svg").write(svg.toString());
+	fileName.setExtension("svg").write(svg.toString());
 }
+
+mixin main!(funopt!makesvg);
